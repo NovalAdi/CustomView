f7bc6c0b1f3e6e22c49448be6124e75039bff35f
NovalAdi <novaladiperasetya@gmail.com>
2022-08-01 08:29:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/customview/CircleView.kt
@@ -0,0 +1,18 @@
+package com.example.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class CircleView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
+
+}
